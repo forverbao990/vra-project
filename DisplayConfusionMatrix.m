fd245954b181d1ece98765737b7eebf59b89515a
BaoDuong <thaibao990@gmail.com>
2017-07-12 23:31:33 +0700

    add more function

---
+++ DisplayConfusionMatrix.m
@@ -0,0 +1,9 @@
+function DisplayConfusionMatrix(confMat, digits)
+% Display the confusion matrix in a formatted table.
+% Convert confusion matrix into percentage form
+confMat = bsxfun(@rdivide,confMat,sum(confMat,2));
+for idx = 1:numel(digits)
+    fprintf('%-9s',   [digits(idx) '      |']);
+    fprintf('%-9.2f', confMat(idx,:));
+    fprintf('\n')
+end
